5b2ab211800230ebeb198a4aca965d8a928b9c3a
dx ddd <oyunaydash@gmail.com>
2025-10-27 20:54:07 +0700

    Master m

--- point 3/ElecScooter.cs
+++ point 3/ElecScooter.cs
@@ -19,6 +19,61 @@
         {
             Console.WriteLine($"Имеется ли сиденье: {(Seat == true ? "Да" : "Нет")}");
         }
+
+        public static void Point2ForElecScooter()
+        {
+            Dictionary<string, ElecScooter> dictionary = new()
+            {
+                {"First", new ElecScooter()},
+                { "Second", new ElecScooter(true)},
+                {"Third", new ElecScooter(false)}
+            };
+
+            Console.WriteLine("\nПервая коллекция: ");
+            foreach (var keys in dictionary) Console.WriteLine($"{keys.Key} {keys.Value}");
+            Console.WriteLine();
+
+            int n = 2;
+            if (n < dictionary.Count)
+            {
+                for (int i = 0; i < n; i++) dictionary.Remove(dictionary.First().Key);
+            }
+
+            dictionary.Add("A", new ElecScooter());
+            dictionary.TryAdd("B", new ElecScooter(true));
+            dictionary["C"] = new();
+
+
+            Console.WriteLine("\nПервая коллекция после удаления: ");
+            foreach (var keys in dictionary) Console.WriteLine($"{keys.Key} {keys.Value}");
+            Console.WriteLine();
+
+            Stack<ElecScooter> stack = [];
+
+            Console.WriteLine("\nВторая коллекция: ");
+            foreach (var key in dictionary)
+            {
+                stack.Push(key.Value);
+            }
+            foreach (var s in stack)
+            {
+                Console.WriteLine(s);
+            }
+
+            var ToFind = new ElecScooter();
+
+            n = 0;
+            if (stack.Contains(ToFind))
+            {
+                foreach (var key in stack)
+                {
+                    if (key.Equals(ToFind)) break;
+                    n++;
+                }
+            }
+            Console.WriteLine($"\nИндекс искомого: {n}");
+
+        }
     }
 
 }
